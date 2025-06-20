005a679aad14f762dad9e9e959f1b3ae84729bc6
Tarik Aybey <tarikeyup.aybey45@gmail.com>
2025-06-20 11:56:06 +0300

    second commit

--- color_queue.py
+++ color_queue.py
@@ -1,10 +1,10 @@
 from ai import *
 from travel import knowledge
 
-colors = ["black", "white", "blue", "green", "purple"]
+colors = ["black", "white", "blue", "purple"]
 symbols = []
 
-for i in range(5):
+for i in range(4):
     for c in colors:
         symbols.append(Symbol(f"{c}{i}"))
 
@@ -16,19 +16,19 @@ for c in colors:
         Symbol(f"{c}1"),
         Symbol(f"{c}2"),
         Symbol(f"{c}3"),
-        Symbol(f"{c}4"),
+
     ))
 
 for c in colors:
-    for i in range(5):
-        for j in range(5):
+    for i in range(4):
+        for j in range(4):
             if i != j:
                 knowledge.add(Implication(
                     Symbol(f"{c}{i}"),
                     Not(Symbol(f"{c}{j}"))
                 ))
 
-for i in range(5):
+for i in range(4):
     for c1 in colors:
         for c2 in colors:
             if c1 != c2:
@@ -38,6 +38,22 @@ for i in range(5):
                 ))
 
 knowledge.add(Or(
-    And(),
+And(Symbol("black0"), Symbol("white1"), Not(Symbol("blue2")), Not(Symbol("purple3"))),
+    And(Symbol("black0"), Symbol("blue2"), Not(Symbol("white1")), Not(Symbol("purple3"))),
+    And(Symbol("black0"), Symbol("purple3"), Not(Symbol("white1")), Not(Symbol("blue2"))),
+    And(Symbol("white1"), Symbol("blue2"), Not(Symbol("black0")), Not(Symbol("purple3"))),
+    And(Symbol("white1"), Symbol("purple3"), Not(Symbol("black0")), Not(Symbol("blue2"))),
+    And(Symbol("blue2"), Symbol("purple3"), Not(Symbol("black0")), Not(Symbol("white1")))
 
 ))
+
+knowledge.add(And(
+    Not(Symbol("white0")),
+    Not(Symbol("black1")),
+    Not(Symbol("blue2")),
+    Not(Symbol("purple3"))
+))
+
+for  s in symbols:
+    if model_check(knowledge, s):
+        print(s)
